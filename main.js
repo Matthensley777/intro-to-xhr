7f46f362be3a4a1906942525266cafe796709c8c
Matt Hensley <matt.hensley777@gmail.com>
2017-03-06 19:57:42 -0600

    one more

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
-console.log("hello");
 
+function executeThisCodeAfterFileLoaded(){
+	console.log("it WORKED!!")
+
+}
+function executeThisCodeAfterFileFails(){
+	console.log("boooo");
+
+}
+
+
+var myRequest = new XMLHttpRequest();
+myRequest.addEventListener("load", executeThisCodeAfterFileLoaded);
+myRequest.addEventListener("error", executeThisCodeAfterFileFails);
+myRequest.open("GET", "dinosaurs.json");
+myRequest.send();
+console.log("myRequest", myRequest);
